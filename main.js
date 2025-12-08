55ce8beda6249212703cf702cc217a0632d1153e
ogg1996 <9migoAhri@gmail.com>
2025-12-08 16:55:56 +0900

    배포를 위한 수정

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import authRouter from '../routes/auth.js';
-import boardRouter from '../routes/board.js';
-import postRouter from '../routes/post.js';
-import imgRouter from '../routes/img.js';
-import activityRouter from '../routes/activity.js';
-import introduceRouter from '../routes/introduce.js';
-import { specs, swaggerUi } from '../swagger.js';
+import authRouter from './routes/auth.js';
+import boardRouter from './routes/board.js';
+import postRouter from './routes/post.js';
+import imgRouter from './routes/img.js';
+import activityRouter from './routes/activity.js';
+import introduceRouter from './routes/introduce.js';
+import { specs, swaggerUi } from './swagger.js';
 
 const app = express();
 
